a9430ea7f3a8b4eea8f5cf4eecfcb7f2bb312e26
Omer Akram <om26er@gmail.com>
2018-12-17 20:23:44 +0500

    Make the application publishable to store

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
-from PySide2.QtWidgets import QApplication, QLabel
 
-app = QApplication(sys.argv)
-label = QLabel("<font color=red size=40>Hello World!</font>")
-label.show()
-app.exec_()
+from PySide2 import QtWidgets
 
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    label = QtWidgets.QLabel("<font color=red size=40>Hello World!</font>")
+    label.show()
+    app.exec_()
